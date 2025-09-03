836b2d705e84bcc552a44a8731cba054369ad2f2
r0ss-benit3Z <benitezrosa600@gmail.com>
2025-09-03 19:01:55 -0300

    .

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -3,6 +3,7 @@ import Logo from "./_logo"
 import PerfilUsuario from "./_perfilUsuario"
 import Buscador from "./_buscador"
 import './Header.css'
+
 const Header = () => {
   return (
     <div className="header">
